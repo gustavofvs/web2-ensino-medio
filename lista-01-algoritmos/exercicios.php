dc3f80e3954c8193892caebf9f426c2168b527ac
Gustavo Fernandes Vieira da Silva <gustavofernandesvs@gmail.com>
2026-02-25 22:52:55 -0300

    add initial PHP file for exercise implementation

---
+++ lista-01-algoritmos/exercicios.php
@@ -0,0 +1,86 @@
+<?php
+
+// ------------------------------
+// Exercício 1
+// Somar dois números e multiplicar o resultado pelo primeiro número
+// ------------------------------
+
+    $n1 = 13;
+    $n2 = 22;
+
+    $result = ($n1 + $n2) + $n1;
+
+    echo "Result:". $result
+
+
+// ------------------------------
+// Exercício 2
+// Ler três números (Val1, Val2, Val3), calcular a média e exibir o resultado
+// ------------------------------
+
+// ------------------------------
+// Exercício 3
+// Receber um valor, calcular e mostrar 15% deste valor
+// ------------------------------
+
+// ------------------------------
+// Exercício 4
+// Receber um valor, calcular e mostrar 5% e 50% deste valor
+// ------------------------------
+
+// ------------------------------
+// Exercício 5
+// Ler dois números, calcular o quadrado de cada um, somar os quadrados e mostrar o resultado
+// ------------------------------
+
+// ------------------------------
+// Exercício 6
+// Ler a velocidade em m/s, calcular e exibir em km/h
+// ------------------------------
+
+// ------------------------------
+// Exercício 7
+// Ler altura e peso, calcular IMC e exibir para o usuário
+// ------------------------------
+
+// ------------------------------
+// Exercício 8
+// Receber um valor, calcular e mostrar um desconto de 9%
+// ------------------------------
+
+// ------------------------------
+// Exercício 9
+// Receber valor de um produto, calcular desconto de 7%, exibir valor original, desconto e valor com desconto
+// ------------------------------
+
+// ------------------------------
+// Exercício 10
+// Calcular e apresentar o volume de uma caixa retangular: volume = comprimento x largura x altura
+// ------------------------------
+
+// ------------------------------
+// Exercício 11
+// Receber um valor, calcular e mostrar um desconto de 27%
+// ------------------------------
+
+// ------------------------------
+// Exercício 12
+// Receber dois valores, calcular e apresentar a área de um trapézio
+// ------------------------------
+
+// ------------------------------
+// Exercício 13
+// Ler 3 notas, calcular média ponderada (pesos: 2, 3, 5)
+// ------------------------------
+
+// ------------------------------
+// Exercício 14
+// Receber valor de produto, acrescentar 16%, dividir em 10 parcelas, mostrar valor da parcela e valor total
+// ------------------------------
+
+// ------------------------------
+// Exercício 15
+// Receber distância (km) e combustível (litros), calcular e imprimir consumo médio: Consumo médio = Km / litros
+// ------------------------------
+
+?>
